be4b3006c1b883d1cfec967f12ba3f8dd1cc0e42
baker-james <bakerjames1995@gmail.com>
2019-07-28 13:41:15 +0100

    Add consistant state to CorrilationID
    Plus ClientID

--- build.go
+++ build.go
@@ -5,21 +5,34 @@ import (
 	"encoding/binary"
 )
 
-func buildPayload(clientId string) []byte {
+type requestBuilder struct {
+	corrId int
+	clientId string
+}
+
+func NewRequestBuilder(clientId string) *requestBuilder {
+	return &requestBuilder{
+		clientId: clientId,
+	}
+}
+
+func (builder *requestBuilder) BuildPayload() []byte {
 	var body bytes.Buffer
-	var corrId int
+	defer func() {
+		builder.corrId++
+	}()
 
 	key, version, detail := buildApiVersions()
 
 	body.Write(key)
 	body.Write(version)
 
-	bCorrId := intToBigEndianSlice(corrId, 32)
+	bCorrId := intToBigEndianSlice(builder.corrId, 32)
 	body.Write(bCorrId)
 
-	bLenClientId := intToBigEndianSlice(len(clientId), 16)
+	bLenClientId := intToBigEndianSlice(len(builder.clientId), 16)
 	body.Write(bLenClientId)
-	body.WriteString(clientId)
+	body.WriteString(builder.clientId)
 
 	body.Write(detail)
 	lenBody := intToBigEndianSlice(body.Len(), 32)
@@ -35,8 +48,6 @@ func intToBigEndianSlice(val, size int) []byte {
 		binary.BigEndian.PutUint16(slice, uint16(val))
 	case 32:
 		binary.BigEndian.PutUint32(slice, uint32(val))
-	case 64:
-		binary.BigEndian.PutUint64(slice, uint64(val))
 	}
 
 	return slice
